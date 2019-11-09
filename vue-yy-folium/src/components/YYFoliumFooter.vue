92418af32164a004b849b8e502421910c2c9a0c2
minyeong <bmy89960212@gamil.com>
2019-11-10 06:07:55 +0900

    finish migration basic template to vue

--- vue-yy-folium/src/components/YYFoliumFooter.vue
+++ vue-yy-folium/src/components/YYFoliumFooter.vue
@@ -12,13 +12,7 @@
                     <a href="#about">About</a>
                 </li>
                 <li>
-                    <a href="#service">Services</a>
-                </li>
-                <li>
                     <a href="#portfolio">Portfolio</a>
-                </li>
-                <li>
-                    <a href="#review">Reviews</a>
                 </li>
                 <li>
                     <a href="#contact">Contact</a>
