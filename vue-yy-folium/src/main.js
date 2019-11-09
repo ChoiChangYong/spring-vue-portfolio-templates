820514a69d4ef464d4162d18bb105e5e994d3823
ChoiChangYong <chanranhan_@naver.com>
2019-11-09 19:57:58 +0900

    init project & add dependency

--- vue-yy-folium/src/main.js
+++ vue-yy-folium/src/main.js
@@ -7,12 +7,13 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 import App from './App.vue'
 
-import { store } from './store/store'
+import {store} from './store/store'
 
 Vue.config.productionTip = false
 
 new Vue({
-  store,
-  render: h => h(App),
-}).$mount('#app')
+    store,
+    render: h = > h(App),
+}).
+$mount('#app')
 
